b024aaaedbf0f25fb706f86458e903400ae6a04c
Sumanta Kundu <sumantasam1990@gmail.com>
2022-09-20 10:33:13 +0530

    react practice

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -5,24 +5,24 @@ export default class Articles extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            books: [],
+            users: [],
         };
     }
 
     async componentDidMount() {
         await fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
-            .then(booksList => {
-                this.setState({ books: booksList });
+            .then(usersList => {
+                this.setState({ users: usersList });
             }).catch(err => console.log(err));
     }
 
     getProductsHtml() {
-        return this.state.books.map((book) => (
-            <li key={book.id}>
-                {book.name}
-                <p className="red">{book.address.city}</p>
-                <p className="green">{book.address.geo.lat}, {book.address.geo.lng}</p>
+        return this.state.users.map((user) => (
+            <li key={user.id}>
+                {user.name}
+                <p className="red">{user.address.city}</p>
+                <p className="green">{user.address.geo.lat}, {user.address.geo.lng}</p>
             </li>
         ));
     }
